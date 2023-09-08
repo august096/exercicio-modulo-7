4f18cc26f102d9686ef689ec456fb0c445c474cf
Augusto Cesar <augustocz.dev@gmail.com>
2023-09-08 16:27:04 -0400

    foi abreviado o evente.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function validaForm(evente) {
-    evente.preventDefault();
+function validaForm(e) {
+    e.preventDefault();
     let primeiroNumero = document.getElementById('numberA').value;
     let segundoNumero = document.getElementById('numberB').value;
 
     if(parseInt(segundoNumero) > (parseInt(primeiroNumero))) {
         alert('Esta correto')
     } else {
-        alert('O numero campo B tem que ser maior que o B')
+        alert('O numero campo B tem que ser maior que o A')
     }
 }
 
